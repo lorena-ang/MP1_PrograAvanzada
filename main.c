7c410eec1dd569ddd2fcdc7cc1a257da2cc7651c
Carlos Bravo <carlos.bravo@advisorycloud.com>
2022-03-20 21:01:21 -0600

    leer archivo y struct terminada

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
 
 FILE *ptrFile;
 char chr;
@@ -8,105 +9,135 @@ char line[500];
 
 struct _ingredients
 {
-    char *name;
-    int quantity;
+	char name[50];
+	int quantity;
 };
 
 struct _recipe
 {
-    char *name;
-    char *decription;
-    char *categories;
-    char *profile;
-    struct _ingredients *list;
+	char name[50];
+	char decription[500];
+	char categories[500];
+	char profile[500];
+	int numIngridients;
+	struct _ingredients list[50];
 };
 
 int all_space(const char *str)
 {
-    while (*str)
-    {
-        if (!isspace(*str++))
-        {
-            return 0;
-        }
-    }
-    return 1;
+	while (*str)
+	{
+		if (!isspace(*str++))
+		{
+			return 0;
+		}
+	}
+	return 1;
 }
 
 int main()
 {
-    struct _recipe recipes;
-    ptrFile = fopen("Recipes.txt", "r");
-    if (ptrFile != NULL)
-    {
-        printf("File opened.\n");
+	struct _recipe recipes[50];
+	int contRecipes = 0;
 
-        // Use the data within the file.
-        int contRecipes = 0;
-        int state = 0;
+	ptrFile = fopen("Recipes.txt", "r");
+	if (ptrFile != NULL)
+	{
+		printf("File opened.\n");
 
-        while (!feof(ptrFile))
-        {
-            // recipes[contRecipes];
-            while (fgets(line, sizeof(line), ptrFile))
-            {
-                if (all_space(line))
-                {
-                    state = 0;
-                    contRecipes++;
-                    printf("new \n");
-                }
-                else
-                {
-                    switch (state)
-                    {
-                    case 0:
-                        printf("name--");
+		// Use the data within the file.
+		int state = 0;
+		int numIng = 0;
+		int cont = 0;
 
-                        /* Name recipes[contRecipes].name = */
-                        state++;
-                        break;
-                    case 1:
-                        printf("descriptio--");
+		while (!feof(ptrFile))
+		{
+			while (fgets(line, sizeof(line), ptrFile))
+			{
+				cont++;
+				printf("%d -- ", contRecipes);
+				if (all_space(line))
+				{
+					state = 0;
+					numIng = 0;
+					contRecipes++;
+					printf("new");
+				}
+				else
+				{
+					char *res = strchr(line, ':') + 1;
 
-                        /* Description */
-                        state++;
-                        break;
-                    case 2:
-                        /* Categories */
-                        printf("catego--");
+					switch (state)
+					{
+					case 0:
+						printf("name--");
+						strcpy(recipes[contRecipes].name, res);
+						state++;
+						break;
+					case 1:
+						printf("descriptio--");
+						strcpy(recipes[contRecipes].decription, res);
+						state++;
+						break;
+					case 2:
+						printf("catego--");
+						strcpy(recipes[contRecipes].categories, res);
+						state++;
+						break;
+					case 3:
+						printf("profile--");
+						strcpy(recipes[contRecipes].profile, res);
+						state++;
+						break;
+					case 4:
+						/* Ingredients */
 
-                        state++;
-                        break;
-                    case 3:
-                        /* Profile */
-                        printf("descriptio--");
+						if (strncmp(line, "Ingredients", 11) != 0)
+						{
+							printf("ingredi-");
+							recipes[contRecipes].list[numIng].quantity = atoi(res);
+							memcpy(recipes[contRecipes].list[numIng].name, line, res - line - 1);
+							numIng++;
+							recipes[contRecipes].numIngridients = numIng;
+							printf("ingredi-");
+						}
+						break;
+					default:
+						break;
+					}
+				}
+				printf("%s", line);
+			}
+		}
+		if (fclose(ptrFile) == 0)
+		{
+			printf("File closed.\n");
+		}
+		else
+		{
+			printf("Unable to close file.\n");
+		}
+	}
+	else
+	{
+		printf("Unable to open file.\n");
+	}
 
-                        state++;
-                        break;
-                    case 4:
-                        /* Ingredients */
-                        printf("ingredi-");
-                        break;
-                    default:
-                        break;
-                    }
-                }
-                printf("%s", line);
-            }
-        }
-        if (fclose(ptrFile) == 0)
-        {
-            printf("File closed.\n");
-        }
-        else
-        {
-            printf("Unable to close file.\n");
-        }
-    }
-    else
-    {
-        printf("Unable to open file.\n");
-    }
-    return 0;
+	printf("----------------------\n");
+
+	for (int i = 0; i <= contRecipes; i++)
+	{
+		printf("NAME: %s", recipes[i].name);
+		printf("DESCRIPTION: %s", recipes[i].decription);
+		printf("CATEGORIES: %s", recipes[i].categories);
+		printf("PROFILE: %s", recipes[i].profile);
+		printf("INGREDIENTS: %d\n", recipes[i].numIngridients);
+		for (int j = 0; j < recipes[i].numIngridients; j++)
+		{
+			printf("\t%s: %d\n", recipes[i].list[j].name, recipes[i].list[j].quantity);
+		}
+		printf("----------------------\n");
+	}
+
+	return 0;
 }

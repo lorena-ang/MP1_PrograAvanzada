e2022d20a9fdf21f776888472b97e2e91c1b811b
MarcelaBrjs <ada_wit61@hackademy.mx>
2022-03-23 07:24:36 -0600

    modificación a la configuración del display del grafo

--- grafo.py
+++ grafo.py
@@ -1,11 +1,12 @@
+from turtle import dot
 from pyvis.network import Network
 from cProfile import label
 from IPython.core.display import display, HTML
 
-g = Network(height='400px', width='50%', heading='Gráfo')
+g = Network(height='500px', width='500px')
 
 # Abrir archivo
-with open('Edges.txt') as f:
+with open("Edges.txt", "r") as f:
     lines = f.readlines()
     print(lines)
 
@@ -42,31 +43,15 @@ print(edges)
 print(nodes)
 print(labels)
 
-# NODOS
-# Para agregar un solo nodo: g.add_node(1, label='a')
-# Para agregar varios al mismo tiempo:
+# Creación de nodos
 g.add_nodes(nodes, label=labels)
 
-# EDGES
-# Para agregar un solo edge: 
+# Creación de edges
+i = 0
+
 while i < len(lines):
     g.add_edge(edges[i][0], edges[i][1], label=edges[i][2]) 
+    i = i + 1
 
 g.show('grafo.html')
 display(HTML('grafo.html'))
-
-'''
-# NODOS
-# Para agregar un solo nodo: g.add_node(1, label='a')
-# Para agregar varios al mismo tiempo:
-g.add_nodes([1, 2, 3], label=['a', 'b', 'c'])
-
-# EDGES
-# Para agregar un solo edge: 
-g.add_edge(1, 2, label="38") 
-g.add_edge(1, 3, label="37") 
-g.add_edge(2, 3, label="38") 
-
-g.show('grafo.html')
-display(HTML('grafo.html'))
-'''
